93edc585ea0754d9fff62823d4a77e621ab6ab18
ThePalsor <chancyleg@protonmail.com>
2021-03-19 20:10:41 +0100

    initial deployment

---
+++ build/precache-manifest.bfe9ed36628563776b928e0184ff6a77.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bd699883620c62fe9feab4943d3c6a5",
+    "url": "/info802-marketplace/index.html"
+  },
+  {
+    "revision": "9f04de8f0f309cd19ac6",
+    "url": "/info802-marketplace/static/css/2.98705a83.chunk.css"
+  },
+  {
+    "revision": "9978367de5035829d41a",
+    "url": "/info802-marketplace/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9f04de8f0f309cd19ac6",
+    "url": "/info802-marketplace/static/js/2.c8a8d24a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/info802-marketplace/static/js/2.c8a8d24a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9978367de5035829d41a",
+    "url": "/info802-marketplace/static/js/main.7fc48c55.chunk.js"
+  },
+  {
+    "revision": "b832163fa510f26596f1",
+    "url": "/info802-marketplace/static/js/runtime-main.447e63e8.js"
+  }
+]);
